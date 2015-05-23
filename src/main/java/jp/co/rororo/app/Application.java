9bb24ceec73d677b0e830ef0c82df6e6f954cf7c
金井 晴弘 <haruhiro.kanai2308@gmail.com>
2015-05-24 00:14:29 +0900

    added logback.xml lombok

--- src/main/java/jp/co/rororo/app/Application.java
+++ src/main/java/jp/co/rororo/app/Application.java
@@ -2,11 +2,14 @@ package jp.co.rororo.app;
 
 import java.util.Arrays;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan(value = {"jp.co.rororo"})
 public class Application {
@@ -14,12 +17,11 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            log.info(beanName);
         }
     }
 
--- src/main/java/jp/co/rororo/controller/api/HelloController.java
+++ src/main/java/jp/co/rororo/controller/api/HelloController.java
@@ -1,13 +1,19 @@
 package jp.co.rororo.controller.api;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping(value = "api/hello")
 public class HelloController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(method = RequestMethod.GET)
     public String index() {
+        log.info("ApiController");
         return "Greetings from Spring Boot!";
     }
 
