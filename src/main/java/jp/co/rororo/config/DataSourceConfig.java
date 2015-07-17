a5b5e26b23f9309c2fc14cbcbaadd6f90d83de65
金井 晴弘 <haruhiro.kanai2308@gmail.com>
2015-07-18 03:13:58 +0900

    add datasource settings

--- src/main/java/jp/co/rororo/config/DataSourceConfig.java
+++ src/main/java/jp/co/rororo/config/DataSourceConfig.java
@@ -1,7 +1,41 @@
 package jp.co.rororo.config;
 
-import org.springframework.stereotype.Component;
 
-@Component
+/*
+@Configuration
+@ConfigurationProperties(prefix = "rororo.datasource")
+@Data
+*/
 public class DataSourceConfig {
+/*	
+	private String url;
+	
+	private String username;
+	
+	private String password;
+	
+	private String driverClassName;
+	
+	private int maxActive;
+
+	private int initialSize;
+	
+	private int maxIdle;
+	
+	private int maxWait;
+	
+	@Bean
+	public DataSource dataSource() {
+		DataSource dataSource = new DataSource();
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setMaxActive(maxActive);
+		dataSource.setInitialSize(initialSize);
+		dataSource.setMaxIdle(maxIdle);
+		dataSource.setMaxWait(maxWait);
+		return dataSource;
+	}
+*/
 }
--- src/main/java/jp/co/rororo/service/UserService.java
+++ src/main/java/jp/co/rororo/service/UserService.java
@@ -3,15 +3,18 @@ package jp.co.rororo.service;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import jp.co.rororo.entity.User;
 import jp.co.rororo.form.UserForm;
 import jp.co.rororo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@Slf4j
 @Service
 public class UserService extends BaseService {
 	
@@ -25,13 +28,17 @@ public class UserService extends BaseService {
 		return userRepository.findAll();
 	}
 	
+	@Transactional
 	public User saveUser(UserForm userForm) {
 		User user = new User();
 		BeanUtils.copyProperties(userForm, user);
 		user.setSystemColums();
-		return userRepository.save(user);
+		User newUser = userRepository.save(user);
+		log.info("newUser: {}", newUser);
+		return newUser;
 	}
 	
+	@Transactional
 	public User updateUser(Integer userId, UserForm userForm) {
 		User user = userRepository.findOne(userId);
 		BeanUtils.copyProperties(userForm, user);
